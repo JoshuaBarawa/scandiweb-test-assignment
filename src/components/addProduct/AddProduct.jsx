16ca7e713179a1cd25a182bdc1c03609e7a107cd
citraxinc <[citraxinc@gmail.com]>
2022-10-12 13:21:51 +0300

    added error notification

--- src/components/addProduct/AddProduct.jsx
+++ src/components/addProduct/AddProduct.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './addproduct.css'
 import InputField from '.././reUsableComponents/inputField/InputField'
 import SelectField from '.././reUsableComponents/selectField/SelectField'
-import { createProduct } from '../APICalls';
+import { createProduct, getAllProducts } from '../APICalls';
 import { useNavigate, Link } from 'react-router-dom';
 
 
@@ -12,6 +12,7 @@ function AddProduct() {
     const navigate = useNavigate()
 
     const selects = ['DVD', 'Furniture', 'Book']
+
     const [product, setProduct] = useState({
         sku: '',
         name: '',
@@ -23,81 +24,99 @@ function AddProduct() {
         length: null,
         weight: null
     })
-    
+
+    const [error, setError] = useState('')
+    const [skus, setSkus] = useState([])
+    const isRunned = useRef(false);
+
+    useEffect(() => {
+        if (!isRunned.current) {
+            getAllProducts().then(res => {
+                res.data?.products.map(product => setSkus(skus => [...skus, product.sku]))
+            });
+        }
+        return () => {
+            isRunned.current = true;
+        };
+    }, [skus])
+
 
     const handleOnChange = (e) => {
         setProduct({ ...product, [e.target.name]: e.target.value })
     }
 
-    
+
     const handleCreateProduct = (e) => {
         e.preventDefault()
-        createProduct(product).then(() => navigate("/"))
+        skus.includes(product.sku) ?
+            setError("SKU Already Exist!") : createProduct(product).then(() => navigate("/"))
     }
 
 
-   
+
     return (
-        
-     <form id="product_form" onSubmit={handleCreateProduct}>
-        <div id='form_title'>
-            <p id="title">Product Add</p>
-        <div className="form_buttons">
-          <button type='submit' id='submit_button'>Save</button>
-          <Link type='button' id='cancel_button' to='/'>Cancel</Link>
-        </div>
-        </div>
 
-      <div className="form_inputs">
+        <form id="product_form" onSubmit={handleCreateProduct}>
+            <div id='form_title'>
+                <p id="title">Product Add</p>
+                <div className="form_buttons">
+                    <button type='submit' id='submit_button'>Save</button>
+                    <Link type='button' id='cancel_button' to='/'>Cancel</Link>
+                </div>
+            </div>
 
-      <InputField name="sku" id="sku" label="SKU" type='text' required={true}
-                handleChange={handleOnChange} errorMessage={errorMessage} />
+            <p id='skuError'>{error}</p>
+            <div className="form_inputs">
 
-<InputField name="name" id="name" label="Name" type='text' required={true}
-                handleChange={handleOnChange} errorMessage={errorMessage} />
+                <InputField name="sku" id="sku" label="SKU" type='text' required={true}
+                    handleChange={handleOnChange} errorMessage={errorMessage} />
 
 
-<InputField name="price" id="price" label="Price $" type='number' required={true} 
-                handleChange={handleOnChange} errorMessage={errorMessage} />
+                <InputField name="name" id="name" label="Name" type='text' required={true}
+                    handleChange={handleOnChange} errorMessage={errorMessage} />
 
 
-<SelectField name="productType" label='Type Switcher' selects={selects} required={true} 
-                placeholder='Choose product type' handleChange={handleOnChange}  errorMessage={errorMessage} />
+                <InputField name="price" id="price" label="Price $" type='number' required={true}
+                    handleChange={handleOnChange} errorMessage={errorMessage} />
 
-       
 
-          
-    { product?.productType === 'DVD' ?  
+                <SelectField name="productType" label='Type Switcher' selects={selects} required={true}
+                    placeholder='Choose product type' handleChange={handleOnChange} errorMessage={errorMessage} />
 
-        <InputField name="size" id="size" label="Size (MB)" type='number' required={true} 
-        handleChange={handleOnChange} errorMessage={errorMessage} />
 
-           : product?.productType === "Furniture" ?
 
-           <>
-            <InputField name="height" id="height" label="Height (CM)" type='number' required={true} 
-        handleChange={handleOnChange} errorMessage={errorMessage} />
 
-<InputField name="width" id="width" label="Width (CM)" type='number' required={true} 
-        handleChange={handleOnChange} errorMessage={errorMessage} />
+                {product?.productType === 'DVD' ?
 
-<InputField name="length" id="length" label="Length (CM)" type='number' required={true} 
-        handleChange={handleOnChange} errorMessage={errorMessage} />
+                    <InputField name="size" id="size" label="Size (MB)" type='number' required={true}
+                        handleChange={handleOnChange} errorMessage={errorMessage} />
 
-        </>
+                    : product?.productType === "Furniture" ?
 
-           : product?.productType === 'Book' ?
+                        <>
+                            <InputField name="height" id="height" label="Height (CM)" type='number' required={true}
+                                handleChange={handleOnChange} errorMessage={errorMessage} />
 
-           <InputField name="weight" id="weight" label="Weight (KG)" type='number' required={true}
-        handleChange={handleOnChange} errorMessage={errorMessage} />
-                : ''
-        }
-          
-                
+                            <InputField name="width" id="width" label="Width (CM)" type='number' required={true}
+                                handleChange={handleOnChange} errorMessage={errorMessage} />
+
+                            <InputField name="length" id="length" label="Length (CM)" type='number' required={true}
+                                handleChange={handleOnChange} errorMessage={errorMessage} />
+
+                        </>
+
+                        : product?.productType === 'Book' ?
+
+                            <InputField name="weight" id="weight" label="Weight (KG)" type='number' required={true}
+                                handleChange={handleOnChange} errorMessage={errorMessage} />
+                            : ''
+                }
+
+
             </div>
 
 
-            </form>
+        </form>
 
     )
 }
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import './productlist.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { getAllProducts, deleteProduct } from '../APICalls';
 
 
@@ -12,7 +12,7 @@ function ProductList() {
 
     useEffect(() => {
         if (!isRunned.current) {
-            getAllProducts().then(res => res.data.products? setProducts(res.data?.products) : products) 
+            getAllProducts().then(res => res.data.products ? setProducts(res.data?.products) : products)
 
         }
         return () => {
@@ -22,43 +22,43 @@ function ProductList() {
 
 
 
-const handleSelectProduct = (product) =>{
-    let newArr = [...selected, product]
-    setSelected(newArr)
-}
+    const handleSelectProduct = (product) => {
+        let newArr = [...selected, product]
+        setSelected(newArr)
+    }
 
-const handleDeleteProducts = () => {
-    selected.map(item => (deleteProduct(item.id)));
-    setProducts(products.filter(value => !selected.includes(value)))
-  }
+    const handleDeleteProducts = () => {
+        selected.map(item => (deleteProduct(item.id)));
+        setProducts(products.filter(value => !selected.includes(value)))
+    }
 
     return (
         <div className='product_section'>
 
 
             <div id='form_title'>
-                        <p id="title">Product List</p>
-                    <div className="form_buttons">
+                <p id="title">Product List</p>
+                <div className="form_buttons">
                     <Link type='button' id='add_button' to='/add-product'>ADD</Link>
                     <button type='submit' id='delete_button' onClick={() => handleDeleteProducts()}>MASS DELETE</button>
-                    </div>
+                </div>
             </div>
 
 
             <div className="products">
-                {products.length < 1? "No Products Available..." : 
-                products.map((product) =>
-                    <div className='product_card' key={product.id}>
-                        <input type="checkbox" className="delete-checkbox" onClick={() => handleSelectProduct(product)}/>
-                        <div className="card-details">
-                        <p>{product.sku}</p>
-                        <p>{product.name}</p>
-                        <p>{parseInt(product.price).toFixed(2)} $</p>
-                        <p>{product.productType === 'DVD'? `Size: ${product.size} MB` 
-                        :product.productType === 'Furniture'? `Dimensions: ${product.height}x${product.width}x${product.length} ` 
-                        :product.productType === 'Book'? `Weight: ${product.weight} KG`:''}</p>
-                        </div>
-                </div>)}
+                {products.length < 1 ? "No Products Available..." :
+                    products.map((product) =>
+                        <div className='product_card' key={product.id}>
+                            <input type="checkbox" className="delete-checkbox" onClick={() => handleSelectProduct(product)} />
+                            <div className="card-details">
+                                <p>{product.sku}</p>
+                                <p>{product.name}</p>
+                                <p>{parseInt(product.price).toFixed(2)} $</p>
+                                <p>{product.productType === 'DVD' ? `Size: ${product.size} MB`
+                                    : product.productType === 'Furniture' ? `Dimensions: ${product.height}x${product.width}x${product.length} `
+                                        : product.productType === 'Book' ? `Weight: ${product.weight} KG` : ''}</p>
+                            </div>
+                        </div>)}
             </div>
         </div>
     )
